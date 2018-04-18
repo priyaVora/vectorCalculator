b1f7603800491e39f58eb5a6c2bd5f67f0ae18ee
priyaVora <prvora89@gmail.com>
2018-04-18 08:29:41 -0600

    Added a junit test for dotProducts! Dot Product operation works!

--- Vector_Calculator/src/test/java/VectorCalculatorTest.java
+++ Vector_Calculator/src/test/java/VectorCalculatorTest.java
@@ -59,10 +59,11 @@ public class VectorCalculatorTest {
 		a_data[1][0] = 2;
 
 		b_data[0][0] = -4;
+
 		b_data[1][0] = 3;
 
-		expectedData[0][0] = -4;
-		expectedData[1][0] = 5;
+		expectedData[0][0] = 4;
+		expectedData[1][0] = -1;
 
 		a.setCurrentVector(a_data);
 		b.setCurrentVector(b_data);
@@ -80,11 +81,52 @@ public class VectorCalculatorTest {
 
 	@Test
 	public void dotProduct1() {
-		VectorCalculatorTest cal = new VectorCalculatorTest();
-		// Vector a = new Vector("A", );
-		// Vector b = new Vector();
+		VectorCalculator cal = new VectorCalculator();
+		Vector a = new Vector("A", 2, 1, VectorOrientation.VERTICAL);
+		Vector b = new Vector("B", 2, 1, VectorOrientation.VERTICAL);
 
-		assertFalse(true);
+		double[][] a_data = new double[2][1];
+		double[][] b_data = new double[2][1];
+		double expectedDotProduct = 20.0;
+
+		a_data[0][0] = 3;
+		a_data[1][0] = 1;
+
+		b_data[0][0] = 9;
+		b_data[1][0] = -7;
+
+		a.setCurrentVector(a_data);
+		b.setCurrentVector(b_data);
+
+		int sizeA = a_data.length * a_data[0].length;
+		int sizeB = a_data.length * b_data[0].length;
+
+		double[] dataA = new double[sizeA];
+		double[] dataB = new double[sizeB];
+		int count = 0;
+		for (int i = 0; i < a_data.length; i++) {
+			for (int j = 0; j < a_data[i].length; j++) {
+				dataA[count] = a_data[i][j];
+				count++;
+			}
+		}
+		int count2 = 0;
+		for (int i = 0; i < b_data.length; i++) {
+			for (int j = 0; j < b_data[i].length; j++) {
+				dataB[count2] = b_data[i][j];
+				count2++;
+			}
+		}
+
+		double resultDotProduct = cal.dotProduct(dataA, dataB);
+		if (resultDotProduct == expectedDotProduct) {
+			assertTrue(true);
+			System.out.println("Expected: " + expectedDotProduct);
+			System.out.println("Result: " + resultDotProduct);
+		} else {
+			assertTrue(false);
+		}
+
 	}
 
 	@Test
